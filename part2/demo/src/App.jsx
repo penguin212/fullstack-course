9d9ef4c65bec97f5690f1e3372a51f1170de1f3e
Dylan Huber <dhuber06@gmail.com>
2025-06-23 17:29:38 -0700

    demo changing notes

--- part2/demo/src/App.jsx
+++ part2/demo/src/App.jsx
@@ -1,14 +1,45 @@
+import { useState } from 'react'
+import Note from './components/Note'
+
 const App = (props) => {
-  const { notes } = props
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: false,
+      id: String(notes.length + 1)
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
 
-export default App
+export default App 
